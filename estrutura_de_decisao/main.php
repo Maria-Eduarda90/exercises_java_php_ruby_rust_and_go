94c178bdeaffe99edd0c0bdbb1a8dbd7d52a51e2
meyh <meyh@gmail.com>
2023-10-25 13:42:20 -0300

    estrutura de decisão

---
+++ estrutura_de_decisao/main.php
@@ -0,0 +1,11 @@
+<?php
+$n1 = 10;
+$n2 = 10;
+
+if ($n1 > $n2) :
+    echo "$n1 é maior que $n2";
+elseif ($n1 < $n2) :
+    echo "$n1 é menor que $n2";
+else :
+    echo "$n1 e $n2 são iguais";
+endif;
---
+++ estrutura_de_decisao/switch.php
@@ -0,0 +1,13 @@
+<?php
+
+switch($var1 = 30){
+    case 10:
+        echo "$var1 é igual a 10";
+        break;
+    case 20:
+        echo "$var1 é igual a 20";
+        break;
+    default:
+        echo "$var1 não é igual a 10 e nem 20";
+        break;
+}
