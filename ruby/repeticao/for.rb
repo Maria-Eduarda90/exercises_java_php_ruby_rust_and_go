9fe22bc1331a7fa3bb0a000c57c93917007140ab
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-12-26 13:09:04 -0300

    repetição

---
+++ ruby/repeticao/for.rb
@@ -0,0 +1,13 @@
+nomes = ["Maria", "João", "Carla", "Cesar"]
+
+for nome in nomes do
+    puts nome
+end
+
+puts "__________________________"
+
+dict = {nome: "Diego", idade: 35, altura: 1.73}
+
+for k, v in dict do
+    puts "#{k}: #{v}"
+end
---
+++ ruby/repeticao/while.rb
@@ -0,0 +1,7 @@
+nomes = ["Maria", "João", "Carla", "Cesar"]
+
+count = 0
+while count < nomes.size do
+    puts nomes[count]
+    count += 1
+end
