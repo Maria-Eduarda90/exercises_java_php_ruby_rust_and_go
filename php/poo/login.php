9809fa73a742ee92beb95bab590fe887df01b267
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-18 21:21:35 -0300

    poo

---
+++ php/poo/login.php
@@ -0,0 +1,38 @@
+<?php
+class Login {
+    private $email;
+    private $senha;
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setEmail($e){
+        $email = filter_var($e, FILTER_SANITIZE_EMAIL);
+        $this->email = $email;
+    }
+
+    public function getSenha(){
+        return $this->senha;
+    }
+
+    public function setSenha($s){
+        $this->senha = $s;
+    }
+
+    public function logar(){
+        if($this->email == "teste@gmail.com" and $this-> senha == "123456"){
+            echo "Logado com sucesso";
+        }
+        else echo "Dados invalidos";
+    }
+}
+
+$logar = new Login();
+$logar->setEmail("teste@gmail.com");
+$logar->setSenha("123456");
+$logar->logar();
+
+echo "<br>";
+
+echo $logar->getEmail();
