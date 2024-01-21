e21da2e4acec07bd2687616eb9ffe57940e78031
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-21 17:13:48 -0300

    tratamento de erro

---
+++ php/poo/error_handling.php
@@ -0,0 +1,20 @@
+<?php
+
+class Newsletter {
+
+    public function cadastrarEmail($email){
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            throw new Exception("Este email é invalido", 1);
+        } else {
+            echo "Email cadastrado com sucesso";
+        }
+    }
+}
+
+$newsletter = new Newsletter();
+
+try {
+    $newsletter->cadastrarEmail("contato@");
+} catch(Exception $e) {
+ echo $e->getMessage();
+}
