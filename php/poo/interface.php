d58075a4786bd77a14975350305f3454927eae7f
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-21 17:14:13 -0300

    interface

---
+++ php/poo/interface.php
@@ -0,0 +1,26 @@
+<?php
+
+interface Crud {
+    public function create();
+    public function read();
+    public function update();
+    public function delete();
+}
+
+class Noticia implements Crud {
+    public function create(){
+
+    }
+
+    public function read(){
+        
+    }
+
+    public function update(){
+        
+    }
+
+    public function delete(){
+        
+    }
+}
